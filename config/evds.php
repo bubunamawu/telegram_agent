485a1a67f1a891e95a1327223b12e2b1e8240907
bubunamawu <bubunamawu@gmail.com>
2020-04-16 21:16:12 +0000

    Update evds.php

--- config/evds.php
+++ config/evds.php
@@ -3,6 +3,9 @@
 return [
     'Agent' => [
         'WAEC' => \App\Conversations\SellWaec::class,
+        'PBECE' => \App\Conversations\SellVoucher::class,
+        'NMC' => \App\Conversations\SellVoucher::class,
+        'TEU' => \App\Conversations\SellVoucher::class,
         'ASUPA' => \App\Conversations\SellASupa::class
     ],
     'Ordinary' => ['WAEC']
